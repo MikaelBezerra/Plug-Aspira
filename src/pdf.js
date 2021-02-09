2907c5bb701e70f57f2134359618c48411a882d6
MikaelBezerra <mikaelbezerra@outlook.com.br>
2021-02-09 18:16:02 -0300

    config

--- src/pdf.js
+++ src/pdf.js
@@ -4,8 +4,6 @@ const Path = require("path")
 const Axios = require("axios")
 const env = require("./config/confi_doc")
 
-
-
 async function downloadPdf() {
     const url = (env.Download.P_URL)
     const path = Path.resolve(__dirname, "doc", "code.pdf")
